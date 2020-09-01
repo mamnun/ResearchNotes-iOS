c2b96914bbb07228badb22ad4d719df87cb7d354
Max Bhuiyan <mamnun.bhuiyan@caltex.com.au>
2020-09-02 08:01:51 +1000

    Initial project

---
+++ ResearchNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ResearchNotes
+//
+//  Created by Max Bhuiyan on 2/9/20.
+//  Copyright © 2020 Max Bhuiyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
